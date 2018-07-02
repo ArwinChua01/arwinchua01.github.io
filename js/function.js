4bc765dfe5734aa1cec8e5976ae575e3e8f16729
ArwinChua01 <36938961+ArwinChua01@users.noreply.github.com>
2018-07-02 02:45:58 +0200

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,4 @@
+function showAlert(){
+    var txt = "Oops! This part is still on development. Thanks for visiting my site! Please provide feedbacks on my facebook account for further improvements.\n-Arwin Chua, Developer";
+    alert(txt);
+    }
